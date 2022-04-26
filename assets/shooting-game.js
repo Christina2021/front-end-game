b847f4d01bc7210e4298a3d89bb67ecbb2aa5860
Christina2021 <christina.shiroma.01@gmail.com>
2022-04-25 22:14:25 -0500

    Add bullet functionality

--- assets/shooting-game.js
+++ assets/shooting-game.js
@@ -1,8 +1,35 @@
 console.log(gameStart)
 
-var cat = document.querySelector("#cat");
-var catPosition = parseInt(getComputedStyle(cat).left);
+let gameBox = document.querySelector("#game-box")
+let cat = document.querySelector("#cat");
+let catPosition = parseInt(getComputedStyle(cat).left);
 
+let bulletSpeed = 75;
+
+class Bullet {
+    constructor() {
+        this.startingPosition = catPosition + 27.5,
+        this.currentYPosition = 25;
+        this.bulletBody = document.createElement("div");
+    }
+    create() {
+        this.bulletBody.classList.add("bullet")
+        this.bulletBody.style.left = `${this.startingPosition}px`
+        gameBox.append(this.bulletBody)
+    }
+    moveUp() {
+        this.currentYPosition += 15;
+        this.bulletBody.style.bottom = `${this.currentYPosition}px`
+
+        if(this.currentYPosition >= 400) {
+            return false
+        } else {
+            return true
+        }     
+    }
+}
+
+// Moving the game piece left and right
 function moveCat(event) {
     if (event.key === "ArrowLeft") {
         moveCatLeft()
@@ -10,7 +37,6 @@ function moveCat(event) {
         moveCatRight()
     }
 }
-
 function moveCatLeft() {
     if(catPosition > 0) {
         catPosition = catPosition - 25;
@@ -24,6 +50,27 @@ function moveCatRight() {
     }
 }
 
+
+//Shooting Items
+function fireBullet(event) {
+    if(event.key === " ") {
+        var bullet = new Bullet;
+        bullet.create()
+
+        let movingBullet = setInterval(
+            function() {
+                let move = bullet.moveUp()
+                
+                if(!move) {
+                    clearInterval(movingBullet)
+                }
+            }
+        ,bulletSpeed)
+    }
+}
+
+
 if (gameStart) {
-    document.addEventListener("keydown", moveCat)
+    document.addEventListener("keydown", moveCat);
+    document.addEventListener("keydown", fireBullet)
 }
