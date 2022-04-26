03f64756664541e36a20266f941fe9b7425cd9f9
Christina2021 <christina.shiroma.01@gmail.com>
2022-04-25 20:22:43 -0500

    Starter files

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+console.log("Test")
+
+/* To-dos:
+-starting game
+
+New file for game:
+    Must include:
+    -box area
+    -item to move on bottom of screen
+    -items coming down from screen
+    -scoreboard/lives
+    -shooting item
+*/
